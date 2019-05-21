6aae4c1eea120a2559133b348aeca06f04e7f68a
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-05-20 23:29:28 -0400

    all except the last fail else statement seem to be working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,20 +5,31 @@ module.exports = {
   get,
 };
 
-const item = ({
-  "name": "",
-  "durability": "",
-  "enahncement": ""
-})
 
 
 function succeed(item) {
-  return { ...item };
+  const enhancement = item.enhancement;
+  const level = item.level;
+  if(level === 20) {
+   return {...item};
+  } else {
+return {...item, enhancement: enhancement + 1}
+  }
 }
 
 function fail(item) {
-  return { ...item };
-}
+  const enhancement = item.enhancement;
+  const level = item.level;
+  const durability = item.durability;
+  if(enhancement < 15) {
+    return {...item, durability: durability - 5}
+  } else if (enhancement >= 15)        {
+    return {...item, durability: durability - 10}
+  } else {
+    (level > 16) 
+      return {...item, level: level - 1}
+    }
+  }
 
 function repair(item) {
   return { ...item, durability: 100};
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,6 +2,9 @@ const enhancer = require('./enhancer.js');
 // test away!
 
 //testing passed 
+// function can be named test or it 
+//add an x in fron to skip test tex xit('should return something')
+//can add toodo ex: it.todo('something, something)
 test('we are up and running', () => {
     expect(true).toBe(true);
 })
@@ -16,15 +19,104 @@ describe('./enhancer.js', () => {
            
         const item = ({
         "name": "crossbow",
-        "durability": "52",
-        "enahancement": "unlimited bows"
+        "durability": 10,
         })
 
         //name of the imported file has to be referenced for the variable
         //took awhile to learn that haha!
        const repair = enhancer.repair(item)
        expect(repair.durability).toBe(100);
+        })
+
+
+        //only trying to return item that is the same hmm
+    describe('succeed(item)', () => {
+        it('should return a new item that is modified by 1', () => {
+            
+            const item = ({
+                "name": "crossbow",
+                "enhancement": 5,
+                "durability": 10,
+                "level": 19
+            });
+        
+
+            const actual = enhancer.succeed(item)
+
+            
+            expect(actual.enhancement).toEqual(6); //working
+            
+            })
+
+        it('should return same old same old because it is level 20', () => {
+            const item = ({
+                "name": "crossbow",
+                "durability": 10,
+                "enhancement": 5,
+                "level": 20
+            });
+
+            const actual = enhancer.succeed(item)
+
+            expect(actual.enhancement).toEqual(5) //working
+        })
+    
+    describe('fail(item', () => {
+        it('should return durability +5 because enhancemnt is less than 15', () => {
+            const item = ({ 
+                    "name": "crossbow",
+                    "durability": 8,
+                    "enhancement": 5,
+                    "level": 20
+            })
+
+            const actual = enhancer.fail(item)
+
+            //pass in props I want to change 
+            expect(actual.durability).toEqual(3)//seems to be working 
+        })
+
+        it('should return durability decreased by 10 because enhancement is 15+', () => {
+            const item = ({ 
+                "name": "crossbow",
+                "durability": 30,
+                "enhancement": 20,
+                "level": 20
+        })
+
+        const actual = enhancer.fail(item)
+
+        expect(actual.durability).toEqual(20) //seems to be working 
+        })
+
+
+
+        it('should return enhancement level -1 because enhancement level is greater than 16', () => {
+            const item = ({ 
+                "name": "crossbow",
+                "durability": 30,
+                "enhancement": 20,
+                "level": 30
+        })
+
+        const actual = enhancer.fail(item)
+
+        expect(actual.level).toEqual(29); //not sure why this isn't working, seems right 
 
         })
     })
+
+
+
+
+        })
+
+    })
+
+  
+     
+    
+
 })
+    
+
